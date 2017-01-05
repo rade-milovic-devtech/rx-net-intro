3a85ce61438e4519b0da3e0cb753f2ef9e0e4028
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-05 22:53:41 +0100

    Adds Where operator example.

---
+++ src/Operators/Filtering/WhereOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace WhereOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            observable.Where(number => number % 2 == 0)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
