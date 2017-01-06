5b6c81d457eb88617120a47b39c3b0389f4830f2
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 20:45:48 +0100

    Adds Last operator example.

---
+++ src/Operators/Filtering/LastOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace LastOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            observable.LastAsync(number => number > 4)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
