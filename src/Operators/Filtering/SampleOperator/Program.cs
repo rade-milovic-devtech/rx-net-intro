9e27f06693c157a0cb13b3c263ec078eaa770e43
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 21:51:17 +0100

    Adds Sample operator example.

---
+++ src/Operators/Filtering/SampleOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace SampleOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromMilliseconds(150));
+
+            observable.Sample(TimeSpan.FromSeconds(1))
+                .Timestamp()
+                .Subscribe(next => Console.WriteLine($"{next.Timestamp}: {next.Value}"));
+
+            Console.Read();
+        }
+    }
+}
