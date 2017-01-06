b1f1ef58770da04f2a5df9718a3afcc866da683a
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 20:19:57 +0100

    Adds Distinct operator example.

---
+++ src/Operators/Filtering/DistinctOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace DistinctOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 1, 2, 3, 4, 4, 4, 5 };
+            var observable = numbers.ToObservable();
+
+            observable.Distinct()
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
