d1cc4fcf9e8a1aa5c54af866de6b3678bdb0beea
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 21:58:02 +0100

    Adds Throttle operator example.

---
+++ src/Operators/Filtering/ThrottleOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ThrottleOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1));
+
+            observable.Throttle(TimeSpan.FromMilliseconds(1500))
+                .Timestamp()
+                .Subscribe(next => Console.WriteLine($"{next.Timestamp}: {next.Value}"));
+
+            Console.Read();
+        }
+    }
+}
