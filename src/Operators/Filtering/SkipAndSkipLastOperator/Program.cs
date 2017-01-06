82cd7ae08831a02806cf9638f622d92c2967911e
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 21:16:51 +0100

    Adds Skip and SkipLast operators example.

---
+++ src/Operators/Filtering/SkipAndSkipLastOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace SkipOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            Console.WriteLine("Skip:");
+            observable.Skip(3)
+                .Subscribe(Console.WriteLine);
+
+            Console.WriteLine();
+
+            Console.WriteLine("SkipLast:");
+            observable.SkipLast(3)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
