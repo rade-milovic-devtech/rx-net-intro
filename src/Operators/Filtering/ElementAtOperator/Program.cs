ff7e559e5e75cbe2d8ef11d7f15e89a5265b1e98
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 19:58:54 +0100

    Adds ElementAt operator example.

---
+++ src/Operators/Filtering/ElementAtOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace ElementAt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            observable.ElementAt(1)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
