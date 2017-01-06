8e582eecbf432ac03c71ca8657aeba3670469153
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 20:07:37 +0100

    Adds IgnoreElements operator example.

---
+++ src/Operators/Filtering/IgnoreElementsOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace IgnoreElementsOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            observable.IgnoreElements()
+                .Subscribe(
+                    Console.WriteLine,
+                    () => Console.WriteLine("Completed")
+                );
+        }
+    }
+}
