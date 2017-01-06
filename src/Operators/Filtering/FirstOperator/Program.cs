97a1f5cc4135bce4d685728ccdeadb35a16a06c7
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 20:34:58 +0100

    Adds First operator example.

---
+++ src/Operators/Filtering/FirstOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace FirstOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            observable.FirstAsync(number => number > 4)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
