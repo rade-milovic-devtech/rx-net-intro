d0e994bf833d5986617ca480fd15b785156eb7cf
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-06 21:22:38 +0100

    Adds Take and TakeLast operators example.

---
+++ src/Operators/Filtering/TakeAndTakeLastOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace TakeAndTakeLastOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            Console.WriteLine("Take:");
+            observable.Take(3)
+                .Subscribe(Console.WriteLine);
+
+            Console.WriteLine();
+
+            Console.WriteLine("TakeLast:");
+            observable.TakeLast(3)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
