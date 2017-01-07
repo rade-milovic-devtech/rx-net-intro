ec682aad82c7daa72ffd6e44bb5b10d37afaa93b
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 15:35:47 +0100

    Adds GroupBy operator example.

---
+++ src/Operators/Transforming/GroupByOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace GroupByOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromMilliseconds(100))
+                .Take(10);
+
+            observable.GroupBy(timestamp => timestamp % 3)
+                .SelectMany(group =>
+                    group.Max()
+                        .Select(value => new { group.Key, Value = value})
+                )
+                .Subscribe(maxValueGroup =>
+                    Console.WriteLine($"{maxValueGroup.Key} -> {maxValueGroup.Value}")
+                );
+
+            Console.Read();
+        }
+    }
+}
