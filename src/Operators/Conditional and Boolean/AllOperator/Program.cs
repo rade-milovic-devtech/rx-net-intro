b2e86007a570db33603fc4e95a922170b5f58b53
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 11:44:00 +0100

    Adds All operator example.

---
+++ src/Operators/Conditional and Boolean/AllOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace AllOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {1, 2, 3, 4, 5};
+            var observable = numbers.ToObservable();
+
+            observable.All(number => number <= 5)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
