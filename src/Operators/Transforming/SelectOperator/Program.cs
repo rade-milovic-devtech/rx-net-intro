0199190cfd9de41dc6d9d76bd100ccfe56362fa2
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 15:18:14 +0100

    Adds Select operator example.

---
+++ src/Operators/Transforming/SelectOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace SelectOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 10);
+            var observable = numbers.ToObservable();
+
+            observable.Select(number => number + 3)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
