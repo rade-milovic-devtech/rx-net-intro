e147b9d565314ad4bda8fd01a492bf508c0969db
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 20:34:19 +0100

    Adds Merge operator example.

---
+++ src/Operators/Combining/MergeOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace MergeOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable1 = Observable.Interval(TimeSpan.FromMilliseconds(250)).Take(3);
+            var observable2 = Observable.Interval(TimeSpan.FromMilliseconds(150)).Take(5)
+                .Select(i => i + 100);
+
+            observable1.Merge(observable2)
+                .Subscribe(Console.WriteLine);
+
+            Console.Read();
+        }
+    }
+}
