b8cf80c1d4eac6b8022cb2d3d89e9271bbc0ae17
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 12:40:43 +0100

    Adds Amb operator example.

---
+++ src/Operators/Conditional and Boolean/AmbOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace AmbOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var subject1 = new Subject<int>();
+            var subject2 = new Subject<int>();
+
+            subject1.Amb(subject2)
+                .Subscribe(Console.WriteLine);
+
+            subject1.OnNext(1);
+            subject2.OnNext(2);
+            subject1.OnNext(3);
+            subject2.OnNext(4);
+            subject1.OnNext(5);
+            subject2.OnNext(6);
+        }
+    }
+}
