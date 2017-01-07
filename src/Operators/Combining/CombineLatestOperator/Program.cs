730c4a652890a47be7652c80dee85f5dc0269983
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 21:49:39 +0100

    Adds CombineLatest operator example.

---
+++ src/Operators/Combining/CombineLatestOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace CombineLatestOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable1 = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5);
+            var observable2 = Observable.Interval(TimeSpan.FromMilliseconds(250)).Take(10);
+
+            observable1.CombineLatest(
+                    observable2,
+                    (left, right) => left + right
+                )
+                .Timestamp()
+                .Subscribe(timestampedValue =>
+                    Console.WriteLine($"{timestampedValue.Value}: {timestampedValue.Timestamp}")
+                );
+
+            Console.Read();
+        }
+    }
+}
