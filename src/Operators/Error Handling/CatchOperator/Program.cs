f30da8dcc496617ed4237e2370c4da30e6da09c8
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 22:12:23 +0100

    Adds Catch operator example.

---
+++ src/Operators/Error Handling/CatchOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace CatchOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var subject = new Subject<int>();
+
+            subject.Catch(Observable.Empty<int>())
+                .Finally(() => Console.WriteLine("Finished"))
+                .Subscribe(
+                    Console.WriteLine,
+                    exception => Console.WriteLine(exception.Message),
+                    () => Console.WriteLine("Completed")
+                );
+
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnError(new Exception("Error"));
+        }
+    }
+}
