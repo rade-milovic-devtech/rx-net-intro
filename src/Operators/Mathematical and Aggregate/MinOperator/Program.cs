f54692ca2416f420bf2be48b61da5f87587afbeb
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 12:52:26 +0100

    Adds Min operator example.

---
+++ src/Operators/Mathematical and Aggregate/MinOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace MinOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {101, 45, 78, 93, 33};
+            var observable = numbers.ToObservable();
+
+            observable.Min()
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
