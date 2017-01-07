3ca095bb39ccbf6df50e82c212842aeae5c96de6
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 16:11:26 +0100

    Adds Buffer operator example.

---
+++ src/Operators/Transforming/BufferOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace BufferOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(10);
+
+            observable.Buffer(3)
+                .Subscribe(buffer =>
+                    Console.WriteLine($"Buffer of [{string.Join(", ", buffer)}] @ {DateTime.Now}"),
+                    () => Console.WriteLine("Completed")
+                );
+
+            Console.Read();
+        }
+    }
+}
