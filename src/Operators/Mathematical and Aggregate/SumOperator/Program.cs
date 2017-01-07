3a106fdcd37e76f70447f3e7cdffa85b02570b05
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 13:00:24 +0100

    Adds Sum operator example.

---
+++ src/Operators/Mathematical and Aggregate/SumOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace SumOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {101, 45, 78, 93, 33};
+            var observable = numbers.ToObservable();
+
+            observable.Sum()
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
