c91cc295518b48004bf82a22b57b79e5422641e1
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 16:27:46 +0100

    Adds Window operator example.

---
+++ src/Operators/Transforming/WindowOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace WindowOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(10);
+
+            var windowIndex = 0;
+            observable.Window(3)
+                .Subscribe(
+                    window => {
+                        Console.WriteLine("--- Starting new window ---");
+                        var windowName = $"Window {++windowIndex}";
+                        window.Subscribe(
+                            value => Console.WriteLine($"{windowName}: {value}"),
+                            () => Console.WriteLine($"{windowName}: completed")
+                        );
+                    },
+                    () => Console.WriteLine("Completed")
+                );
+
+            Console.Read();
+        }
+    }
+}
