fc50d8a982c25ee8cbbc896bea431be8adc0a786
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 13:11:13 +0100

    Adds Count operator example.

---
+++ src/Operators/Mathematical and Aggregate/CountOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace CountOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {101, 45, 78, 93, 33};
+            var observable = numbers.ToObservable();
+
+            observable.Count()
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
