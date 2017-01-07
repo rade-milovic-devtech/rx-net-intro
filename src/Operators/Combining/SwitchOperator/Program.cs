72af25d23397b352b886af1dc5ea823ffa55fc2b
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 20:54:31 +0100

    Adds Switch operator example.

---
+++ src/Operators/Combining/SwitchOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace SwitchOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Observable.Range(0, 3)
+                .Select(value => Observable.Range(value, 3))
+                .Switch()
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
