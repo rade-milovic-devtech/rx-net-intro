34f723abdab0908df2d67e0f6cab6d03141d569c
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 12:55:30 +0100

    Adds Max operator example.

---
+++ src/Operators/Mathematical and Aggregate/MaxOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace MaxOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {101, 45, 78, 93, 33};
+            var observable = numbers.ToObservable();
+
+            observable.Max()
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
