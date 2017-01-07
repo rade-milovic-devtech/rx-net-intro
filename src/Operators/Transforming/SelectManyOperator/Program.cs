f8ca9ab0d32522f8899fe5591cafbb05c552ec7c
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 15:22:30 +0100

    Adds SelectMany operator example.

---
+++ src/Operators/Transforming/SelectManyOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace SelectManyOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 3);
+            var observable = numbers.ToObservable();
+
+            observable.SelectMany(number => Observable.Range(1, number))
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
