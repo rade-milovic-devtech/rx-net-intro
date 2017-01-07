c5b9f95030b7d152e03bc5d197d9cf45c2c12bb6
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 11:54:31 +0100

    Adds Contains operator example.

---
+++ src/Operators/Conditional and Boolean/ContainsOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ContainsOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {1, 2, 3, 4, 5};
+            var observable = numbers.ToObservable();
+
+            observable.Contains(2)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
