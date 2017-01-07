53fe3710710e4ff04c5ee46301ca170c13837d3d
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 13:06:23 +0100

    Adds Average operator example.

---
+++ src/Operators/Mathematical and Aggregate/AverageOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace AverageOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {101, 45, 78, 93, 34};
+            var observable = numbers.ToObservable();
+
+            observable.Average()
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
