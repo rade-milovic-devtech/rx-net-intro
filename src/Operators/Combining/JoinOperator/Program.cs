42094f210c53730e7ecfecb94fa292c849a7ccf8
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 20:27:47 +0100

    Adds Join operator example.

---
+++ src/Operators/Combining/JoinOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace JoinOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable1 = Observable.Interval(TimeSpan.FromMilliseconds(250)).Take(3);
+            var observable2 = Observable.Interval(TimeSpan.FromMilliseconds(150)).Take(6)
+                .Select(number => char.ConvertFromUtf32((int) number + 97));
+
+            observable1.Join(
+                    observable2,
+                    _ => Observable.Never<Unit>(),
+                    _ => Observable.Never<Unit>(),
+                    (left, right) => new { Left = left, Right = right }
+                )
+                .Subscribe(zip =>
+                    Console.WriteLine($"{{ {zip.Left}, {zip.Right} }}")
+                );
+
+            Console.Read();
+        }
+    }
+}
