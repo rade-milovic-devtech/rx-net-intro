bab8c18a69034e85d8ac122bf0e230080fe6b7cf
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 12:01:49 +0100

    Adds SequenceEquals operator example.

---
+++ src/Operators/Conditional and Boolean/SequenceEqualsOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace SequenceEqualsOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers1 = new[] {1, 2, 3, 4};
+            var observable1 = numbers1.ToObservable();
+            var numbers2 = new[] {1, 2, 3, 4, 5};
+            var obserbable2 = numbers2.ToObservable();
+
+            observable1.SequenceEqual(obserbable2)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
