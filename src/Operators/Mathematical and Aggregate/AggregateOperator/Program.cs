99b4c4ac3fb7760ceca0d23539b5afe39389e76b
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 13:17:51 +0100

    Adds Aggregate operator example.

---
+++ src/Operators/Mathematical and Aggregate/AggregateOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace AggregateOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {101, 45, 78, 93, 33};
+            var observable = numbers.ToObservable();
+
+            observable.Aggregate((accumulated, current) => accumulated + current)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
