82d4529361feb2965ff9569122b00053531619ba
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 21:02:28 +0100

    Adds StartWith operator example.

---
+++ src/Operators/Combining/StartWithOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace StartWithOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Observable.Range(0, 3)
+                .StartWith(-3, -2, -1)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
