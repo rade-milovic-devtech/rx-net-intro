95dd53f943c3e722f985a49b99ef3214bd85f012
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 19:36:47 +0100

    Adds And/Then/When operators example.

---
+++ src/Operators/Combining/AndThenWhenOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace AndThenWhenOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable1 = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5);
+            var observable2 = Observable.Interval(TimeSpan.FromMilliseconds(250)).Take(10);
+            var observable3 = Observable.Interval(TimeSpan.FromMilliseconds(150)).Take(14);
+
+            Observable.When(
+                observable1.And(observable2)
+                    .And(observable3)
+                    .Then((first, second, third) =>
+                        new { First = first, Second = second, Third = third }
+                    )
+            )
+            .Subscribe(zip =>
+                Console.WriteLine($"{{ {zip.First}, {zip.Second}, {zip.Third} }}")
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
