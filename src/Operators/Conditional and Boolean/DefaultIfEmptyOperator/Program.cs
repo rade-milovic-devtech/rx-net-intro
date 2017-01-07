8eadbd0a5fbc19d7c8021481ebefdad62703154f
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 12:07:51 +0100

    Adds DefaultIfEmpty operator example.

---
+++ src/Operators/Conditional and Boolean/DefaultIfEmptyOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace DefaultIfEmptyOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Empty<int>();
+
+            observable.DefaultIfEmpty(50)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
