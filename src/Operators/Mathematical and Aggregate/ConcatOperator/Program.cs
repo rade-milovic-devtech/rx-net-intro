e1f44bd206657bdf74c9e873061989fb5729076a
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 13:30:32 +0100

    Adds Concat operator example.

---
+++ src/Operators/Mathematical and Aggregate/ConcatOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ConcatOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable1 = Observable.Range(1, 5);
+            var observable2 = Observable.Range(6, 5);
+
+            observable1.Concat(observable2)
+                .Subscribe(Console.WriteLine);
+        }
+    }
+}
