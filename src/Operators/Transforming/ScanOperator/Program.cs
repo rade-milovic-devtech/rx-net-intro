eb1493d0455f2373ea5b2f61d6aafa60ea6b9f4f
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-07 15:44:27 +0100

    Adds Scan operator example.

---
+++ src/Operators/Transforming/ScanOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace ScanOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var subject = new Subject<int>();
+            subject.Subscribe(number => Console.WriteLine($"Subject: {number}"));
+
+            subject.Scan(0, (accumulated, current) => accumulated + current)
+                .Subscribe(number => Console.WriteLine($"Scan: {number}"));
+
+            subject.OnNext(1);
+            subject.OnNext(2);
+            subject.OnNext(3);
+            subject.OnCompleted();
+        }
+    }
+}
