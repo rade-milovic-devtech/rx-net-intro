a9bb08615a9bfbfcc69900a704e031ede6d82d4f
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:59:44 +0100

    Adds Range operator example.

---
+++ src/Operators/Creating/RangeOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace RangeOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Range(0, 3);
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+        }
+    }
+}
