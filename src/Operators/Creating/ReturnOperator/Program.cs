f321dd62448e76f7f1f14a21739a33ef83f99f17
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:54:22 +0100

    Adds Return operator example.

---
+++ src/Operators/Creating/ReturnOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ReturnOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Return("test");
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+        }
+    }
+}
