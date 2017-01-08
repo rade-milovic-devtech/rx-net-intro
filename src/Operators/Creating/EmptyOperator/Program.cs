d5b19f4a13da7b1a631cac9bdce45ecdc4922e24
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:39:54 +0100

    Adds Empty operator example.

---
+++ src/Operators/Creating/EmptyOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace EmptyOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Empty<string>();
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+        }
+    }
+}
