950abc4284b318b644d9dddcbe7ba5228879eb20
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 13:21:34 +0100

    Adds Interval operator example.

---
+++ src/Operators/Creating/IntervalOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace IntervalOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromMilliseconds(250));
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+
+            Console.Read();
+        }
+    }
+}
