1ddeabb632b57bc8aac922c25c05e492e80bc695
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 23:31:28 +0100

    Adds ForEach operator example.

---
+++ src/Operators/Converting/ForEachOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ForEachOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5);
+
+            observable.ForEachAsync(value =>
+                Console.WriteLine($"received {value} @ {DateTime.Now}")
+            );
+
+            Console.Read();
+        }
+    }
+}
