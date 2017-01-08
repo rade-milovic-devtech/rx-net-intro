dde66e11d147f21d3d40531d85061054b0bd6366
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 11:36:50 +0100

    Adds Delay operator example.

---
+++ src/Operators/Utility/DelayOperator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace DelayOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(5).Timestamp();
+
+            observable.Subscribe(
+                value => Console.WriteLine($"original : {value}"),
+                () => Console.WriteLine("original Completed")
+            );
+
+            observable.Delay(TimeSpan.FromSeconds(2))
+                .Subscribe(
+                    value => Console.WriteLine($"delayed : {value}"),
+                    () => Console.WriteLine("delayed Completed")
+                );
+
+            Console.Read();
+        }
+    }
+}
