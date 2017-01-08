89ced7e02be50742a223103b3571658c6d2ac579
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:49:35 +0100

    Adds Throw operator example.

---
+++ src/Operators/Creating/ThrowOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace ThrowOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Throw<string>(new Exception("Error"));
+
+            observable.Subscribe(
+                Console.WriteLine,
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+        }
+    }
+}
