b4a672d30328dc5a276bb54cb35b53818c982923
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:11:31 +0100

    Adds Timeout operator example.

---
+++ src/Operators/Utility/TimeoutOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace TimeoutOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromMilliseconds(100))
+                .Take(3)
+                .Concat(Observable.Interval(TimeSpan.FromSeconds(2)));
+
+            observable.Timeout(TimeSpan.FromSeconds(1))
+                .Subscribe(
+                    Console.WriteLine,
+                    Console.WriteLine,
+                    () => Console.WriteLine("Completed")
+                );
+
+            Console.Read();
+        }
+    }
+}
