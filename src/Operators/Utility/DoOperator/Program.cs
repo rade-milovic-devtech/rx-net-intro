927b2106b7ed39a179ce19ed409ce7554c5e9b28
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 11:23:34 +0100

    Adds Do operator example.

---
+++ src/Operators/Utility/DoOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace DoOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(3);
+
+            observable.Do(
+                value => Console.WriteLine($"Logging OnNext({value}) @ {DateTime.Now}"),
+                exception => Console.WriteLine($"Logging OnError({exception.Message}) @ {DateTime.Now}"),
+                () => Console.WriteLine($"Logging OnCompleted() @ {DateTime.Now}")
+            )
+            .Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("completed")
+            );
+
+            Console.Read();
+        }
+    }
+}
