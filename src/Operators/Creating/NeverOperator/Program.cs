418b7e3a5764b5b343b4824c662dbc920f327881
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:45:58 +0100

    Adds Never operator example.

---
+++ src/Operators/Creating/NeverOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace NeverOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Never<string>();
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+        }
+    }
+}
