668d7f57b9b5add6af1bb2dccaf9e23680364886
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:57:14 +0100

    Adds Repeat operator example.

---
+++ src/Operators/Creating/RepeatOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace RepeatOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Repeat("test", 3);
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+        }
+    }
+}
