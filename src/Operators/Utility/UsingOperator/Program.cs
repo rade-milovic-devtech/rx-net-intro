c4f65979d44e182744e0711c573f1be7a3798f56
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:25:23 +0100

    Adds Using operator example.

---
+++ src/Operators/Utility/UsingOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace UsingOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Using(
+                () => new TimeIt("Subscription Timer"),
+                timeIt => Observable.Interval(TimeSpan.FromSeconds(1))
+            );
+
+            observable.Take(3)
+                .Subscribe(
+                    Console.WriteLine,
+                    () => Console.WriteLine("Completed")
+                );
+
+            Console.Read();
+        }
+    }
+}
---
+++ src/Operators/Utility/UsingOperator/TimeIt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UsingOperator
+{
+    public class TimeIt : IDisposable
+    {
+        private readonly string name;
+        private readonly Stopwatch watch;
+
+        public TimeIt(string name)
+        {
+            this.name = name;
+            watch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            watch.Stop();
+            Console.WriteLine($"{name} took {watch.Elapsed}");
+        }
+    }
+}
