0b1085fbd90c78a64622f55d7ad0a6e9cd20cfee
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 23:51:38 +0100

    Adds To operators example.

---
+++ src/Operators/Converting/ToOperators/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+namespace ToOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Create<int>(observer =>
+            {
+                observer.OnNext(0);
+                observer.OnNext(1);
+                observer.OnError(new Exception("Error!"));
+
+                return Disposable.Empty;
+            });
+
+            Console.WriteLine("To IEnumerable<T>");
+
+            var enumerable = observable.ToEnumerable();
+            try
+            {
+                foreach (var value in enumerable)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("To Event");
+
+            var observableEvent = observable.ToEvent();
+            try
+            {
+                observableEvent.OnNext += Console.WriteLine;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("To Task");
+
+            var task = observable.ToTask();
+            try
+            {
+                Console.WriteLine(task.Result);
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
+        }
+    }
+}
