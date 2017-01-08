a5bddf3f22cbd288817bdf96ea150accba43e024
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 13:26:33 +0100

    Adds Timer operator example.

---
+++ src/Operators/Creating/TimerOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace TimerOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Timer(TimeSpan.FromSeconds(1));
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+
+            Console.Read();
+        }
+    }
+}
