b9fa9921d5ded503a3b6b13d8b6cedfcdf580628
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 22:53:08 +0100

    Adds Publish and Connect operators example.

---
+++ src/Operators/Connecting/PublishAndConnectOperators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace PublishAndConnectOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Publish();
+
+            observable.Connect();
+            observable.Subscribe(value =>
+                Console.WriteLine($"first subscription: {value}")
+            );
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            observable.Subscribe(value =>
+                Console.WriteLine($"second subscription: {value}")
+            );
+
+            Console.Read();
+        }
+    }
+}
