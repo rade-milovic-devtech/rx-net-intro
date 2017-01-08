ca1b23d8cb7d8c754957be4749fd190bb74a8d01
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:03:08 +0100

    Adds Timestamp and TimeInterval operators example.

---
+++ src/Operators/Utility/TimestampAndTimeIntervalOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace TimestampAndTimeIntervalOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1)).Take(3);
+
+            observable.Timestamp()
+                .Subscribe(value => Console.WriteLine($"Timestamp: {value}"));
+
+            observable.TimeInterval()
+                .Subscribe(value => Console.WriteLine($"Time interval: {value}"));
+
+            Console.Read();
+        }
+    }
+}
