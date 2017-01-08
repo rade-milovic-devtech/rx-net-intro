0f4eb24b7f07ea7ef8c27223275845449ea3c202
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 23:07:34 +0100

    Adds RefCount operator example.

---
+++ src/Operators/Connecting/RefCountOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace RefCountOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Do(count => Console.WriteLine($"Publishing {count}"))
+                .Publish()
+                .RefCount();
+
+            var subscription = observable.Subscribe(value =>
+                Console.WriteLine($"subscription: {value}"),
+                () => Console.WriteLine("Completed")
+            );
+
+            Console.WriteLine("Press any key to unsubscribe");
+            Console.Read();
+            subscription.Dispose();
+        }
+    }
+}
