efdfe635800cfd5fef506e79e8598b9f31ac079a
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 23:23:46 +0100

    Adds Replay operator example.

---
+++ src/Operators/Connecting/ReplayOperator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace ReplayOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Interval(TimeSpan.FromSeconds(1))
+                .Take(3)
+                .Publish();
+
+            observable.Connect();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            var otherObservable = observable.Replay();
+            otherObservable.Connect();
+            otherObservable.Subscribe(value =>
+                Console.WriteLine($"first subscription: {value}")
+            );
+
+            otherObservable.Subscribe(value =>
+                Console.WriteLine($"second subscription: {value}")
+            );
+            Console.Read();
+
+            otherObservable.Subscribe(value =>
+                Console.WriteLine($"third subscription: {value}")
+            );
+            Console.Read();
+        }
+    }
+}
