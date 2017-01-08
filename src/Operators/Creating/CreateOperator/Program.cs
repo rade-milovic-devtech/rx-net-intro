58466ba4f27dd7bc30e5d2f116976e28ac0e1fd9
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 12:35:37 +0100

    Adds Create operator example.

---
+++ src/Operators/Creating/CreateOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace CreateOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Create<int>(observer =>
+            {
+                observer.OnNext(0);
+                observer.OnNext(1);
+                observer.OnNext(2);
+                observer.OnCompleted();
+
+                return Disposable.Empty;
+            });
+
+            observable.Subscribe(Console.WriteLine);
+        }
+    }
+}
