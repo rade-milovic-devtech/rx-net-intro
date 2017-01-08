b9dd4338f868165356addfa4a341f4e200d563e7
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 10:44:27 +0100

    Adds Retry operator example.

---
+++ src/Operators/Error Handling/RetryOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace RetryOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Create<int>(observer =>
+            {
+                observer.OnNext(0);
+                observer.OnNext(1);
+                observer.OnNext(2);
+                observer.OnError(new Exception());
+
+                return Disposable.Empty;
+            });
+
+            observable.Retry(2)
+                .Subscribe(
+                    Console.WriteLine,
+                    _ => Console.WriteLine("Error"),
+                    () => Console.WriteLine("Completed")
+                );
+        }
+    }
+}
