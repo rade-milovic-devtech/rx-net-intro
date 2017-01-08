68564f344b098055ed85fca5d8d8b011bc1c6074
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 11:55:01 +0100

    Adds Materialize and Dematerialize operators example.

---
+++ src/Operators/Utility/MaterializeAndDematerializeOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace MaterializeAndDematerializeOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 5);
+            var observable = numbers.ToObservable();
+
+            var materialized = observable.Materialize();
+            materialized.Subscribe(Console.WriteLine);
+
+            var dematerialized = materialized.Dematerialize();
+            dematerialized.Subscribe(Console.WriteLine);
+        }
+    }
+}
