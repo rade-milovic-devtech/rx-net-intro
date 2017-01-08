6328d48ff9ed00a75d0ddccc4263ef47e9256b28
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 13:42:28 +0100

    Adds Start operator example.

---
+++ src/Operators/Creating/StartOperator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace StartOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("On main thread.");
+
+            var observable = Observable.Start(() =>
+            {
+                Console.WriteLine("From backgound thread.");
+                Console.WriteLine("Calculating...");
+                Thread.Sleep(3000);
+                Console.WriteLine("Background work completed.");
+
+                return 100;
+            });
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+
+            observable.Wait();
+        }
+    }
+}
