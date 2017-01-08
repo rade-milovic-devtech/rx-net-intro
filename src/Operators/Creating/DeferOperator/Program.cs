1ea155742a5aed7621c77c4be431f2018a82380d
Rade Milovic <rade.milovic@devtechgroup.com>
2017-01-08 13:48:27 +0100

    Adds Defer operator example.

---
+++ src/Operators/Creating/DeferOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace DeferOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var observable = Observable.Defer(() => Observable.Return("test"));
+
+            observable.Subscribe(
+                Console.WriteLine,
+                () => Console.WriteLine("Completed")
+            );
+        }
+    }
+}
